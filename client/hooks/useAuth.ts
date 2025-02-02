53a918f6328296271caa483cb9753ad3238a1056
David-mwas <dmwas704@gmaIl.com>
2025-02-02 22:04:15 +0300

    safeenv client set up

---
+++ client/hooks/useAuth.ts
@@ -0,0 +1,25 @@
+const useAuthToken = () => {
+  const getItem = () => {
+    if (typeof window !== "undefined") {
+      const token = localStorage?.getItem("safeEnv");
+    //   const chatid = localStorage?.getItem("chatId");
+      return { token }; // Return an object with token and chatid
+    } else {
+      // return { token: null, chatid: null };
+    }
+    // return { token: null }; // Return null if localStorage is not available
+  };
+
+  const clearAuthToken = () => {
+    if (typeof window !== "undefined") {
+      // Remove the token from local storage
+      localStorage.removeItem("safeEnv");
+    //   localStorage.removeItem("chatId");
+    }
+  };
+
+  // Return the token and functions to update and clear it
+  return { clearAuthToken, getItem };
+};
+
+export default useAuthToken;
